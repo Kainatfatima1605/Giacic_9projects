55ef92a6977e506957f7b864cd1c9486bb0a6fde
kainatkhan16 <kainatkhan2516@gmail.com>
2025-04-29 07:36:33 -0700

    2nd project done

---
+++ guess-number/index.py
@@ -0,0 +1,24 @@
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    print("🎯 Welcome to the Guess the Number Game!")
+    print("I have chosen a number between 1 and 100.")
+
+    while True:
+        try:
+            user_guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if user_guess < number_to_guess:
+                print("Too low! Try again.")
+            elif user_guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"🎉 Congratulations! You guessed it in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+guess_the_number()
---
+++ mid-lab/index.py
@@ -0,0 +1,15 @@
+
+
+name = input("Enter your name:  ")
+age = input("Enter your age: ")
+color = input("Enter your favorite colour: ")
+city  = input("Enter  your city: ")
+food = input("What is your favorite food? ")
+job = input("What is your dream job? ")
+hobby = input("what is your favorite hobby ?")
+season = input("What is your favorite season?  ") 
+
+print(f"Hello, {name}! you are {age} years old , & your favorite color is {color}.You live in {city} and love {hobby} as a hobby.")
+print(f"Your favorite food is {food}, and your dream job is to become a {job}.")
+print(f"Your favorite season is {season}.")
+print("Thank you for sharing your information with us! 🥰  ")
